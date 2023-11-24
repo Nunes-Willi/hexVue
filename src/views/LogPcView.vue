d15dfc9453e0726ce2925d0993058aa8e5b1cb6a
BCD15 <bruno69correadias@gmail.com>
2023-11-24 18:35:45 -0300

    LogPc

--- src/views/LogPcView.vue
+++ src/views/LogPcView.vue
@@ -1,3 +1,33 @@
 <template>
-  <h1>Olá</h1>
+  <div class="all">
+    <div class="container">
+      <div class="form"><p>olá</p></div>
+      <div class="image">
+        <img src="../imagens/Group 12.png" alt="hex" style=""/>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.all {
+  background: linear-gradient(180deg, #224948 0%, #061010 100%);
+  width: 100%;
+  height: 100%;
+  padding-bottom: 1000px;
+}
+.container {
+  background-color: red;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-self: center;
+  width: 70%;
+  margin: 250px;
+  height: 100px;
+}
+
+.image {
+    height: 10%;
+}
+</style>
